f05c84b168a1480affed22a4e24057de44b78b72
Vladimir Ivanov <102364412+VladimirIvanovImperial@users.noreply.github.com>
2022-05-09 21:28:54 +0100

    Delete pysoar directory

--- pysoar/__init__.py
+++
@@ -1,5 +0,0 @@
-from pysoar.baseline import baseline
-from pysoar.threshold import threshold
-from pysoar.find_peaks import find_peaks
-from pysoar.event_detection import event_detection
-from pysoar.Translocations import Translocations
